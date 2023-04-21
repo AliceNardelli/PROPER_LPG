663e53681f75e525996f718f776cbf1a197a4a5a
AliceNardelli <alice.nardelli98@gmail.com>
2023-04-21 12:36:58 +0200

    stange.. work everything

--- load_ontology.py
+++ load_ontology.py
@@ -2,3 +2,12 @@ from owlready2 import *
 onto_path.append("/home/alice/")
 onto = get_ontology("http://www.semanticweb.org/alice/ontologies/2023/3/ontology_new#").load()
 print(list(onto.classes()))
+
+
+with onto:
+    class Predicates(Thing):
+        pass
+          
+with onto:
+    class Predicates(Thing):
+        pass
